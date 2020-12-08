7e6f57e2c7b0303ce11477b5fa74efe2ae485361
prataplohar <75601129+prataplohar@users.noreply.github.com>
2020-12-08 18:03:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ class HelloWorld{
     public static void main(String args[])
 	{  
      System.out.println(".....*****Welcome to Java world and test webhook*****.......#####");  
-	    System.out.println("updated 12-08-20");  
     }  
 } 
